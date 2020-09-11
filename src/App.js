34f684bbab66c669949b866c593e848163c3a733
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-09-11 08:33:36 -0700

    Axios request completed. Working on componenet.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { setState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+  //setting inital state here to empty array
+   const [charactersData, setCharactersData] = setState({})
+
+  useEffect( () =>{
+    // axios.get('https://rickandmortyapi.com/api/character')
+    .then(response => {
+      console.log(response.data.results)
+      setCharactersData(response.data.results)  //Array of Objs
+    })
+    .catch( error =>
+      console.log('ERROR when geting data from API: ', error))
+      debugger
+  }
+  ) 
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character charactersData={charactersData}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+export default function Character (props) {
+
+    return (
+        <div className='characterContainer'>
+        <h1>{props.name}</h1>
+        <img src={props.image} alt={props.species}></img>
+        </div>
+    )
+
+}
